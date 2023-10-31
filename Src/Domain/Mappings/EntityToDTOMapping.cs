a71cf55b110e61d77a6a1ac9499dc3056fda6a09
Diego Santos <diegom.santos03@gmail.com>
2023-10-30 21:49:15 -0300

    chore: created mappings and mapper

---
+++ Src/Domain/Mappings/EntityToDTOMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DMS_WhichYourSign_API.Src.Domain.DTOs;
+using DMS_WhichYourSign_API.Src.Infra.Data.Entities;
+
+namespace DMS_WhichYourSign_API.Src.Domain.Mappings
+{
+    public class EntityToDTOMapping : Profile
+    {
+        public EntityToDTOMapping()
+        {
+            CreateMap<WhichYourSign, WhichYourSignResponseDTO>();
+        }
+    }
+}
---
+++ Src/Infra/Data/Mappers/WhichYourSignMapper.cs
@@ -0,0 +1,25 @@
+﻿using DMS_WhichYourSign_API.Src.Infra.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DMS_WhichYourSign_API.Src.Infra.Data.Mappers
+{
+    public class WhichYourSignMapper : IEntityTypeConfiguration<WhichYourSign>
+    {
+        public void Configure(EntityTypeBuilder<WhichYourSign> builder)
+        {
+            builder.ToTable("WhichYourSign");
+
+            builder.HasKey(e => e.SignId);
+
+            builder.Property(e => e.SignName)
+                .HasColumnName("Name");
+
+            builder.Property(e => e.InicialDate)
+                .HasColumnName("InitialDate");
+
+            builder.Property(e => e.FinalDate)
+                .HasColumnName("FinalDate");
+        }
+    }
+}
