685618307fa5acd4c143dd0f7afce1efe5029e76
Diego Santos <diegom.santos03@gmail.com>
2023-10-30 21:48:19 -0300

    chore: created DTO, model and entity

---
+++ Src/Domain/Models/WhichYourSignModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DMS_WhichYourSign_API.Src.Domain.Models
+{
+    public class WhichYourSignModel
+    {
+        public string UserName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Src/Infra/Data/Entities/WhichYourSign.cs
@@ -0,0 +1,10 @@
+﻿namespace DMS_WhichYourSign_API.Src.Infra.Data.Entities
+{
+    public class WhichYourSign
+    {
+        public byte SignId { get; set; }
+        public string SignName { get; set; }
+        public DateTime InicialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+    }
+}
