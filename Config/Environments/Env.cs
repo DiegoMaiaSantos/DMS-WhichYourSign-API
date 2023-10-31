1c050a0d382c008d4b7ff54e1208e06f37f7e932
Diego Santos <diegom.santos03@gmail.com>
2023-10-30 21:45:00 -0300

    chore: created constants and env

---
+++ Config/Environments/Env.cs
@@ -0,0 +1,8 @@
+﻿namespace DMS_WhichYourSign_API.Config.Environments
+{
+    public static class Env
+    {
+        public static readonly Func<string, string> Get = (
+            env) => Environment.GetEnvironmentVariable(env);
+    }
+}
