f64278eedabce920f3e447b5fff84937fe8b3db2
Diego Santos <diegom.santos03@gmail.com>
2023-10-30 21:47:28 -0300

    chore: created service and repository interfaces

---
+++ Src/Domain/Interfaces/Repositories/IWhichYourSignRepository.cs
@@ -0,0 +1,10 @@
+﻿using DMS_WhichYourSign_API.Src.Domain.DTOs;
+using DMS_WhichYourSign_API.Src.Infra.Data.Entities;
+
+namespace DMS_WhichYourSign_API.Src.Domain.Interfaces.Repositories
+{
+    public interface IWhichYourSignRepository
+    {
+        WhichYourSign SearchData(DateTime data);
+    }
+}
